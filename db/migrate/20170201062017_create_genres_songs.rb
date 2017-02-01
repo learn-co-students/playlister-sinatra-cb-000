17007370ccac37cc61f1a049850f5874056e8cfa
drew <andre@vollrath.ca>
2017-01-31 22:22:30 -0800

    Done.

---
+++ db/migrate/20170201062017_create_genres_songs.rb
@@ -0,0 +1,8 @@
+class CreateGenresSongs < ActiveRecord::Migration
+  def change
+    create_table :genres_songs, id: false do |t|
+      t.belongs_to :genre, index: true
+      t.belongs_to :song, index: true
+    end    
+  end
+end
