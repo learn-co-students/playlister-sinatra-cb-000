eb42b6053edcf636111ae9ef62350f29951a13ae
Michael Wahlin <wahlin@gmail.com>
2017-12-26 11:53:25 -0500

    Add index page for songs

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -9,6 +9,9 @@ end
 module ClassSlugifiable
     def find_by_slug(slug)
       slug = slug.split("-").map(&:capitalize).join(" ")
+      slug.gsub!(/The/, "the")
+      slug.gsub!(/With/,"with")
+      slug.gsub!(/\sA\s/," a " )
       self.find_by(name: slug)
     end
 end
