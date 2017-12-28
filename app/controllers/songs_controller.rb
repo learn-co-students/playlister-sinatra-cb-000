f66486c08f87f20761bf965b78f27db4f7951178
Michael Wahlin <wahlin@gmail.com>
2017-12-28 09:30:43 -0500

    Add /songs/new

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,8 +5,30 @@ class SongsController < ApplicationController
       erb :'/songs/index'
     end
 
+    get '/songs/new' do
+      erb :'/songs/new'
+    end
+
     get '/songs/:slug' do
       @song = Song.find_by_slug(params[:slug])
       erb :'/songs/show'
+    end
+
+    post '/songs' do
+      @song = Song.create(name: params["Name"])
+      
+      params["Genre"].each do |genre|
+        @song.genres << Genre.find_by(name: genre)
+      end
+
+      @song.save
+      if @artist = Artist.find_by(name: params["Artist Name"])
+        @artist
+      else
+        @artist = Artist.create(name: params["Artist Name"])
+      end
+
+      @artist.songs << @song
+      redirect "/songs/#{@song.slug}"
     end
 end
