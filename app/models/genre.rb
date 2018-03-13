7f19e887862d160d7f7715aacf4dc87d03090ed7
David Kuruvilla <dapawn@gmail.com>
2018-03-13 17:44:55 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,8 +2,8 @@ class Genre < ActiveRecord::Base
   include Slugifiable::InstanceMethods
   extend Slugifiable::ClassMethods
 
-  has_many :artists, :through => :songs_genres
   has_many :song_genres
   has_many :songs, :through => :song_genres
+  has_many :artists, :through => :songs
 
 end
