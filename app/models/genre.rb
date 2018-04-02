86b2468aa15bb6256f1df9f04686e4f85c0eab51
adam n <adam.nawroc@gmail.com>
2018-04-02 13:34:22 +0000

    genre spec

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,4 +3,20 @@ class Genre < ActiveRecord::Base
   has_many :songs, through: :song_genres
 
   has_many :artists, through: :songs
+
+  def slug
+    # not sure why self.name is better than @name here
+    self.name.downcase.gsub(/\s/, '-')
+  end
+
+  def self.find_by_slug(slug)
+    # reverse the slug to name?
+    slugToName = slug.gsub('-', ' ').gsub(/\w+/, &:capitalize)
+    # slug = slug.gsub(/\w+/, &:capitalize)
+
+
+    Genre.where(name: slugToName).first
+    # binding.pry
+
+  end
 end
