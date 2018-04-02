dfb6f2a23743354ae621d27f32a71fb21541b475
adam n <adam.nawroc@gmail.com>
2018-04-02 12:43:36 +0000

    artist_id col to songs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402123040) do
+ActiveRecord::Schema.define(version: 20180402124031) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20180402123040) do
   create_table "songs", force: :cascade do |t|
     t.string "name"
     t.string "artist"
+    t.integer "artist_id"
   end
 
 end
