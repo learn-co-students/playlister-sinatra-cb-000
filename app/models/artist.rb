d17c6eb3c2fccfc33e5848aee48d0fce5d7c656f
adam n <adam.nawroc@gmail.com>
2018-04-02 12:44:09 +0000

    comment

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,7 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
-
   # i dont understand what im doing.
+  # works, but i wouldnt be able to explain why it does
 
 end
