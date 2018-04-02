45bb2fc074da1a13339c0ec3762b7d03478abac8
adam n <adam.nawroc@gmail.com>
2018-04-02 12:33:35 +0000

    join migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402120039) do
+ActiveRecord::Schema.define(version: 20180402123040) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 20180402120039) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "song_genres", force: :cascade do |t|
+    t.integer "song_id"
+    t.integer "genre_id"
   end
 
   create_table "songs", force: :cascade do |t|
