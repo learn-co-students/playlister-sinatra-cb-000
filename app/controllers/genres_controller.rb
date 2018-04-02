bef996c2b03edca6bdb878be77a47c33ce527567
adam n <adam.nawroc@gmail.com>
2018-04-02 17:34:56 +0000

    genre slug route

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -4,4 +4,19 @@ class GenresController < ApplicationController
 
     erb :'/genres/index'
   end
+
+  get '/genres/:slug' do
+    # @songs = Song.all
+    # this is tested to work..
+    # p Song.all
+
+    # class method.. spelt right, given an arg. arg has contents..
+    # but doesnt return anything..
+    # find by something else?
+    @genre = Genre.find_by_slug(params[:slug])
+    # binding.pry
+
+
+    erb :'/genres/show'
+  end
 end
