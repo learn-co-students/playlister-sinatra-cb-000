9461cc5fa743ddeb74759e3329f0adfa74f3217d
adam n <adam.nawroc@gmail.com>
2018-04-02 17:31:32 +0000

    artist slug route

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,4 +4,19 @@ class ArtistsController < ApplicationController
 
     erb :'/artists/index'
   end
+
+  get '/artists/:slug' do
+    # @songs = Song.all
+    # this is tested to work..
+    # p Song.all
+
+    # class method.. spelt right, given an arg. arg has contents..
+    # but doesnt return anything..
+    # find by something else?
+    @artist = Artist.find_by_slug(params[:slug])
+    # binding.pry
+
+
+    erb :'/artists/show'
+  end
 end
