930de6266f7726b4f70cd533ea93d043f839fdda
Alex Segers <a.segers.dev@gmail.com>
2018-05-22 00:50:24 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180522000604) do
+ActiveRecord::Schema.define(version: 20180522003823) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180522000604) do
 
   create_table "songs", force: :cascade do |t|
     t.string "name"
+    t.integer "artist_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+Artist.destroy_all
+Song.destroy_all
+Genre.destroy_all
+
 bazzi = Artist.create(name: "Bazzi")
 
 honest = Song.create(name: "Honest")
@@ -26,4 +30,6 @@ work.genres << hiphop
 diamonds.genres << pop
 
 rihanna.songs << work
-rihanna.diamonds << diamonds
+rihanna.songs << diamonds
+
+p "Created #{Artist.count} artists, #{Song.count} songs, and #{Genre.count} genres"
