c54b9a6bc2c5f7b6d9b6b803fba3f506672a0c83
Alex Segers <a.segers.dev@gmail.com>
2018-05-22 00:24:47 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,29 @@
-# Add seed data here. Seed your database with `rake db:seed`
+bazzi = Artist.create(name: "Bazzi")
 
+honest = Song.create(name: "Honest")
+mine = Song.create(name: "Mine")
+
+pop = Genre.create(name: "Pop")
+
+##
+honest.genres << pop
+mine.genres << pop
+
+bazzi.songs << honest
+bazzi.songs << mine
+
+#########
+
+rihanna = Artist.create(name: "Rihanna")
+
+work = Song.create(name: "Work")
+diamonds = Song.create(name: "Diamonds")
+
+hiphop = Genre.create(name: "Hip Hop")
+
+##
+work.genres << hiphop
+diamonds.genres << pop
+
+rihanna.songs << work
+rihanna.diamonds << diamonds
