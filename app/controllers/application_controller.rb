f284b358d9937947682246790585a39754555bb5
Alex Segers <a.segers.dev@gmail.com>
2018-05-25 20:12:31 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,3 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 end
-
-=begin --Useful Aliases--
-  alias be="bundle exec "
-  alias bake="bundle exec rake "
-  alias bux="bundle exec tux"
-  alias set="bundle exec rake db:migrate; bundle exec rake db:migrate SINATRA_ENV=test; bundle exec rake db:seed"
-  alias reset="bundle exec rake db:migrate VERSION=0; bundle exec rake db:migrate; bundle exec rake db:migrate SINATRA_ENV=test; bundle exec rake db:seed"
-=end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,7 @@
-require 'pry'
+require 'rack-flash'
 class SongsController < ApplicationController
+  use Rack::Flash
+
   get '/songs' do
     @songs = Song.all
     erb :'songs/index'
@@ -10,16 +12,35 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    binding.pry
     @song = Song.create(name: params[:name])
     @song.artist = Artist.find_or_create_by(name: params[:artist])
     @song.genre_ids = params[:genre_ids]
     @song.save
-    redirect to '/songs/#{@song.slug}'
+
+    flash[:message] = "Successfully created song."
+
+    redirect to "/songs/#{@song.slug}"
   end
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/view'
   end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids = params[:genre_ids]
+    @song.save
+
+    flash[:message] = "Successfully updated song."
+    redirect("/songs/#{@song.slug}")
+  end
+
 end
