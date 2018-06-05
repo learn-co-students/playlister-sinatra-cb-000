413683930521b117d9a7d1a9455d28a7e5f352f4
nichia <nichia@gmail.com>
2018-06-05 00:06:14 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,19 @@ class ApplicationController < Sinatra::Base
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
+  enable :sessions
+  register Sinatra::Flash
+
   get '/' do
     erb :index
+  end
+
+  get '/parse_db' do
+    LibraryParser.parse
+
+    # Flash Message when the db is populated
+    flash[:message] = "Library Parsey Successfully populated database."
+    
+    redirect "/"
   end
 end
---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,21 @@
+class GenresController < ApplicationController
+
+  # Read get: Index page
+  get '/genres' do
+    @genres = Genre.all
+    erb :'/genres/index'
+  end
+
+  # Read get: New genre page
+  get '/genres/new' do
+    erb :'/genres/new'
+  end
+
+  # Read: Show genre's detail page
+  get '/genres/:slug' do
+    @genre = Genre.find_by_slug(params[:slug])
+    erb :'/genres/show'
+  end
+
+
+end
