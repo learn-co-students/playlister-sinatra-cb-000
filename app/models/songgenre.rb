78ca69c66d3c4acfd2a69b1311becd803e9a5994
Sergio Marrocoli <s.marrocoli@gmail.com>
2018-12-19 22:16:14 +0000

    Done.

--- app/models/songgenre.rb
+++ app/models/songgenre.rb
@@ -1,3 +1,4 @@
 class SongGenre < ActiveRecord::Base
-  # belongs_to :artist, :genre
+  belongs_to :artist
+  belongs_to :genre
 end
--- db/migrate/20181219121254_songgenres.rb
+++ db/migrate/20181219121254_songgenres.rb
@@ -1,4 +1,8 @@
 class Songgenres < ActiveRecord::Migration
   def change
+    create_table :song_genres do |t|
+      t.integer :genre_id
+      t.integer :song_id
+    end
   end
 end
