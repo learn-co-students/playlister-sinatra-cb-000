40665ddc09afbd808b2359b03b93ab4fe3a13e06
Sergio Marrocoli <s.marrocoli@gmail.com>
2018-12-19 23:20:00 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,7 +1,16 @@
 class Genre < ActiveRecord::Base
-  has_many :artist_genres
+  # has_many :artist_genres
   has_many :song_genres
-  has_many :songs, through: :song_genre
-  has_many :artists, through: :artist_genre
+  has_many :songs, through: :song_genres
+  has_many :artists, through: :songs
+
+  def slug
+    self.name.downcase.gsub ' ', '-'
+  end
+
+  def self.find_by_slug(input)
+    # binding.pry
+    self.all.detect {|artist| artist.slug == input}
+  end
 
 end
