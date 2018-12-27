afe8316bfc915b394ae64b22eb59990f1a665b41
Sergio Marrocoli <s.marrocoli@gmail.com>
2018-12-27 15:06:55 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,6 +27,7 @@ class SongsController < ApplicationController
   get '/songs/:slug/edit' do
     # binding.pry
     @song = Song.find_by_slug(params[:slug])
+    @genres = Genre.all
     erb :'songs/edit'
   end
 
