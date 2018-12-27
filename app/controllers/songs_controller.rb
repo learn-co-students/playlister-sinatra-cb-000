99e5d1bb3c8fa0f6e0ef10583456f32f39958d14
Sergio Marrocoli <s.marrocoli@gmail.com>
2018-12-27 15:05:33 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,6 +24,22 @@ class SongsController < ApplicationController
     erb :'songs/new'
   end
 
+  get '/songs/:slug/edit' do
+    # binding.pry
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    binding.pry
+    @song = Song.find_by_slug(params[:slug])
+    @song.artist = Artist.find_or_create_by(name: params[:artist_name])
+    session[:update_message] = "Successfully updated song."
+    # @song.genres = params[@genres]
+    @song.save
+    redirect to "/songs/#{@song.slug}"
+  end
+
 
   get '/songs/:slug' do
     # binding.pry
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,27 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
+anthem = Artist.create(name: "Gaslight anthem")
+against_me = Artist.create(name: "Against me!")
+trio = Artist.create(name: "Alkaline trio")
+arms = Artist.create(name: "The Lawrence Arms")
+
+forty_five = Song.create(name: "45")
+cliche = Song.create(name: "Cliche Guevera")
+mercy_me = Song.create(name: "Mercy me")
+escapes = Song.create(name: "Great lakes/Great escapes")
+
+punk = Genre.create(name: "Punk")
+rock = Genre.create(name: "Rock")
+pop = Genre.create(name: "Pop")
+folk = Genre.create(name: "Folk")
+
+
+anthem.songs << forty_five
+cliche.artist = against_me
+trio.songs << mercy_me
+escapes.artist = arms
+
+# anthem.genres << punk
+# rock.artists << against_me
+# trio.genres << pop
+# arms.genres << folk
